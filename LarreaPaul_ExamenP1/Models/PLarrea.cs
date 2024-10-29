5f5e510c782028c931b3dc9a53f72c30f1c2db8c
ItzPaulz <paullarrea@gmail.com>
2024-10-29 07:40:23 -0500

    Mi primer commit, cree clases solicitad

---
+++ LarreaPaul_ExamenP1/Models/PLarrea.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LarreaPaul_ExamenP1.Models
+{
+    public class PLarrea
+    {
+        [Key]
+        [Required]
+        public int idNombre { get; set; }
+        [EmailAddress]
+        public string correo { get; set; }
+        [Required]
+        [MinLength(9)]
+        [Phone]
+        public int celular { get; set; }
+        [MaxLength(3)]
+        public float iq { get; set; }
+        [Required]
+        public bool casado { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime FechaCumple { get; set; }
+
+
+
+    }
+}
